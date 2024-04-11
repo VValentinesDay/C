718a19c7e06c4a6fbab655abd8721fd8686d1355
Valentine <walter9retlaw@gmail.com>
2024-04-11 18:48:58 +0300

    Создание гит репо

---
+++ seminars/sem1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Console.WriteLine("Write sm number wich will be mult by 2 :");
+// int a = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Your result is " + a*2);
+// System.Console.WriteLine();
+// System.Console.WriteLine("Пример хитрой вставки через знак долара");
+// System.Console.WriteLine($"Квадрат числа {a} это {a*a}");
+// System.Console.WriteLine();
+// System.Console.WriteLine("Чтобы закомментировать код используется связка ctrl+/");
+// int b = 10;
+// System.Console.WriteLine();
+// System.Console.WriteLine("Работа с блоком if");
+// if (b == a*b - b) {System.Console.WriteLine(' ');}
+// else{
+//     System.Console.WriteLine("...");
+// }
+
+
+
+//Задача, чтобы при вводе числа выводился ряд от -чилос до число
+System.Console.WriteLine("Write N:");
+int N =  Convert.ToInt32(Console.ReadLine());
+int start =  -N;
+
+while(start<=N){
+    // Console.Write(start + ' '); // Видимо шарп не умеет в конкатенацию чаров и прочего
+     Console.Write(start + " ");
+    start++;
+}
+//Условия и && и или ||
+if (N > 99 && N < 999) {System.Console.WriteLine("Число не трёхзначное");}
+
+
+
+
+
---
+++ seminars/sem2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Функция и метод - оно и то же
+
+void Quad(int a, int b)
+{
+    System.Console.WriteLine($"Результат перемножения {a} на {b} = {a * b}");
+}
+
+int QuadI(int a, int b) {
+    return a*b;
+}
+
+// Quad(78, 6);
+
+System.Console.WriteLine(QuadI(2, 2));
