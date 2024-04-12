4b094e02d8f27e268d5c35cd75407c2152471dc7
Valentine <walter9retlaw@gmail.com>
2024-04-13 00:08:43 +0300

    Массивы

--- seminars/sem2/Program.cs
+++ seminars/sem2/Program.cs
@@ -1,14 +1,17 @@
 ﻿// Функция и метод - оно и то же
 
-void Quad(int a, int b)
-{
-    System.Console.WriteLine($"Результат перемножения {a} на {b} = {a * b}");
-}
+
+// void Quad(int a, int b)
+// {
+//     System.Console.WriteLine($"Результат перемножения {a} на {b} = {a * b}");
+// }
+
+// Quad(78, 6);
+
 
 int QuadI(int a, int b) {
     return a*b;
 }
 
-// Quad(78, 6);
 
 System.Console.WriteLine(QuadI(2, 2));
---
+++ seminars/sem3/Program.cs
@@ -0,0 +1,57 @@
+﻿// Массивы
+
+//Метод слздания массива
+
+int[] CreateArray (int size, int min, int max) {
+    int[] Array = new int[size];
+
+
+    for(int i = 0; i < size; i++){
+        Array[i] = new Random().Next(min, max+1);
+    }
+
+    return Array;
+}
+
+void PrintArray(int[] Array)
+{
+    
+    for(int i = 0; i < Array.Length; i++ ) 
+    {
+        System.Console.Write(Array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+bool Find(int f, int[] a){
+    for(int i =0; i < a.Length; i++)
+    {
+        if(a[i] == f) { 
+            return true;
+            }
+    }
+    return false;
+} 
+
+
+
+
+
+
+System.Console.WriteLine("Количество");
+int a = Convert.ToInt32( Console.ReadLine());
+System.Console.WriteLine("Мин");
+int b = Convert.ToInt32( Console.ReadLine());
+System.Console.WriteLine("Макс");
+int c = Convert.ToInt32( Console.ReadLine());
+
+int[] A = CreateArray(a,b,c);
+
+System.Console.WriteLine("Вывод массива:");
+
+PrintArray(A);
+
+System.Console.WriteLine("Число для поиска");
+int f = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(Find(f, A));
